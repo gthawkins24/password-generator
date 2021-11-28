4c8ef86d29544163d597bfb991ecb47f4c3e3eec
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2021-11-28 12:06:53 -0500

    entered variables, started generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,28 @@
 // Assignment code here
+const numbersList = "0123456789";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const special = "!#$%&'()*+-/<=>?@[]^_`{|}~";
+let lengthPass = "";
+let genPass = "";
 
 
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
 
+// Generate password function, prompt user
+function generatePassword() {
+  // character length user prompt
+  lengthPass = window.prompt("How many characters should your password be? (Should be between 8 and 128 characters)")
+
+  //check if entry was a number
+  if (Number.isNaN(lengthPass)) {
+    window.alert("You must enter a number!");
+    return null;
+  };
+
+  //check if number entered is > 8 and < 128
+}
 // Write password to the #password input
 function writePassword() {
   const password = generatePassword();
